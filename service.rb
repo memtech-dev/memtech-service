019e371b845548a94c3fa412144e316c7f054210
Daniel J. Pritchett <dpritchett@gmail.com>
2011-08-26 15:50:07 -0500

    added start page with link to /tweets

--- service.rb
+++ service.rb
@@ -3,7 +3,16 @@ require 'twitter'
 require 'json'
 
 get '/' do
-    "Hello from Sinatra on Heroku!"
+    home_page = <<-HERE.gsub /^\s+/, ""
+    <h1>Welcome to memtech-service!</h1>
+    <p>
+        Check out the project page on <a href="https://github.com/memtech-dev/memtech-service">github</a>.
+    </p>
+    <h3>API Features</h3>
+    <ul>
+        <li><a href="/tweets">#memtech tweets</a></li>
+    </ul>
+    HERE
 end
 
 get '/tweets' do
