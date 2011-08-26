9bf2425ac14d05975064073b575f4a59f7d2f979
Daniel J. Pritchett <dpritchett@gmail.com>
2011-08-26 15:28:12 -0500

    reworked timestamp to put single JSON serialization call on the outside

--- service.rb
+++ service.rb
@@ -10,7 +10,9 @@ get '/tweets' do
     content_type :json
     tweets = Twitter::Search.new.hashtag("memtech").fetch
 
-    output = { timestamp:   Time.now,
-               tweets:      JSON.generate(tweets) }
+    output = JSON.generate({
+        timestamp:   Time.now,
+        tweets:      tweets
+    })
 end
     
